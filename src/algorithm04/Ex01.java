7a00e65680ebebe2901702a55aed7be9fceb0b17
kim,ji-won <jiwon_kim@gmail.com>
2025-04-04 13:05:57 +0900

    4.4 코딩 테스트 문제 풀이

---
+++ src/algorithm04/Ex01.java
@@ -0,0 +1,41 @@
+package algorithm04;
+
+/**
+ * 링크 https://school.programmers.co.kr/learn/courses/30/lessons/120908
+ */
+
+public class Ex01 {
+	
+	/**
+	 * 문제 설명
+	 * 1. 문자열 a 와 b가 주어질 때
+	 * 2. a 안에 b가 있다면 1 , 없다면 2를 return
+	 */
+	
+	// 풀이 1.
+	// 시간 0.02ms ~ 0.02ms
+	public int solution(String str1, String str2) {
+		
+		if(str1.contains(str2)) {
+			return 1;
+		}
+		
+		return 2;
+	}
+	
+	
+	// 풀이 2. 삼항 연잔사 활용
+	// 시간 0.01ms ~ 0.02ms
+	public int solution1(String str1, String str2) {
+		return str1.contains(str2)? 1 : 2;
+	}
+	
+	public static void main(String[] args) {
+		Ex01 ex01 = new Ex01();
+		
+		System.out.println(ex01.solution1("ab6CDE443fgh22iJKlmn1o", "6CD"));
+		System.out.println(ex01.solution1("ppprrrogrammers", "pppp"));
+		
+	}
+
+}
