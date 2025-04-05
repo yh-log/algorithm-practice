3922fe27e909d796c852b6f525ce0c45f532fe76
kim,ji-won <jiwon_kim@gmail.com>
2025-04-05 17:00:04 +0900

    4.5 코딩테스트 문제 풀이

---
+++ src/algorithm05/Ex01.java
@@ -0,0 +1,48 @@
+package algorithm05;
+
+import java.util.Arrays;
+
+/**
+ * 링크 https://school.programmers.co.kr/learn/courses/30/lessons/120811
+ */
+
+public class Ex01 {
+	
+	/**
+	 * 문제 설명
+	 * 1. 주어진 정수 배열 array가 있을 때
+	 * 2. 중앙값을 찾아 return
+	 *  ※ 배열의 길이는 홀수이다
+	 */
+	
+	// 풀이 1. Arrays.sort() 로 배열 정렬 후 중앙 인덱스 반환
+	// 시간 0.33ms ~0.52 ms
+	public int solution(int[] array) {
+		
+		Arrays.sort(array);
+		
+		return array[array.length / 2];
+	}
+	
+	
+	// 풀이 2. 만약 홀수개가 아닌 짝수개로도 배열을 받는 경우의 풀이
+	public int solution1(int[] array) {
+		
+		Arrays.sort(array);
+		
+		if(array.length % 2 == 0) {
+			return (array[array.length / 2] + array[array.length / 2 - 1] / 2);
+			
+		}else {
+			return array[array.length / 2];
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		Ex01 ex01 = new Ex01();
+		int[] arr = {1, 2, 7, 10, 11};
+		System.out.println(ex01.solution1(arr));
+	}
+
+}
---
+++ src/algorithm05/Ex04.java
@@ -0,0 +1,56 @@
+package algorithm05;
+
+/**
+ * 링크 https://school.programmers.co.kr/learn/courses/30/lessons/120837
+ */
+
+public class Ex04 {
+	
+	/**
+	 * 문제 설명 (그리디 알고리즘 문제)
+	 * 1. 장군 개미는 5, 병졍 개미는 3, 일개미는 1
+	 * 2. 사냥감의 hp에 따라 최소한의 병령을 구성할 경우
+	 * 3. 개미의 수를 return
+	 */
+	
+	// 풀이 1. 나눈 값을 변수에 저장하고, 나머지 값을 hp에 저장하는 방법
+	// 시간 0.01ms ~ 0.02ms
+	public int solution(int hp) {
+			
+		int count = hp / 5;
+		hp %= 5;
+		
+		count += hp / 3;
+		hp %= 3;
+		
+		count += hp;
+		
+		return count;
+	}
+	
+	// 풀이 2. 배열과 반복문으로 풀이
+	// 시간 0.01ms ~ 0.02ms
+	public int solution1(int hp) {
+		
+		int[] power = {5,3,1};
+		int count = 0;
+		
+		
+		for(int num : power) {
+			count += hp / num;
+			hp %= num;
+		}
+		
+		
+		return count;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Ex04 ex04 = new Ex04();
+		System.out.println(ex04.solution(999));
+		
+	}
+
+}
