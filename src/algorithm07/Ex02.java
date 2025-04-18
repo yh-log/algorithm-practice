c219a346a61ccbe6660abd641438a38aaa2a5c9a
kim,ji-won <jiwon_kim@gmail.com>
2025-04-18 10:22:32 +0900

    04.18 코딩테스트 문제풀이

---
+++ src/algorithm07/Ex02.java
@@ -0,0 +1,39 @@
+package algorithm07;
+
+/**
+ * 링크 https://school.programmers.co.kr/learn/courses/30/lessons/120851
+ */
+
+public class Ex02 {
+	
+	/**
+	 * 문제 설명
+	 * 1. 문자열 배열에 숫자의 합을 return
+	 */
+	
+	// 풀이. isdigit 사용
+	// 시간 7.38ms ~ 21.61ms
+	public int solution(String my_string) {
+		
+		int sum = 0;
+		
+		for(int i = 0; i < my_string.length(); i++) {
+			
+			char ch = my_string.charAt(i);
+			
+			if(Character.isDigit(ch)) {
+				System.out.println(ch + " = " + sum);
+				sum += Integer.parseInt(String.valueOf(ch));
+			}
+		}
+		
+		return sum;
+	}
+	
+	
+	public static void main(String[] args) {
+		Ex02 ex02 = new Ex02();
+		System.out.println(ex02.solution("aAb1B2cC34oOp"));
+	}
+
+}
---
+++ src/algorithm07/Ex03.java
@@ -0,0 +1,33 @@
+package algorithm07;
+
+/**
+ * 링크 https://school.programmers.co.kr/learn/courses/30/lessons/120899
+ */
+
+public class Ex03 {
+	
+	/**
+	 * 문제 설명
+	 * 1. 정수 배열의 가장 큰 수와 그 수의 index return
+	 */
+	
+	// 풀이
+	// 시간 0.01ms
+	public int[] solution(int[] array) {
+
+		int max = array[0];
+		
+		int[] arr = {array[0], 0};
+		
+		for(int i = 1; i < array.length; i++) {
+			if(array[i] > max) {
+				max = array[i];
+				arr[0] = max;
+				arr[1] = i;
+			}
+		}
+		
+		return arr;
+	}
+
+}
