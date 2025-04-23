7359b4a4cb15d0ac3c99285be257902d5dfd62b7
yh.kim <yihyun.22222@gmail.com>
2025-04-23 18:20:25 +0900

    04.23 코딩테스트 - x 사이의 개수

---
+++ src/algorithm10/Ex01.java
@@ -0,0 +1,46 @@
+package algorithm10;
+
+import java.util.Arrays;
+
+/**
+ * 링크 https://school.programmers.co.kr/learn/courses/30/lessons/181867
+ */
+
+public class Ex01 {
+	
+	/**
+	 * 문제 설명
+	 * 1. 문자열을 "x"를 기준으로 나눴을 때 
+	 * 2. 각각의 길이를 저장한 배열을 return
+	 */
+	
+	
+	// 풀이
+	// 시간 ms ~ ms
+	public int[] solution(String myString) {
+		
+		
+		
+		String[] arr = myString.split("x", -1);
+		int [] num = new int[arr.length];
+
+		
+		for(int i = 0; i < arr.length; i++) {
+			
+			num[i] = arr[i].length();
+			
+		}
+		
+		return num;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Ex01 ex01 = new Ex01();
+		
+		System.out.println(Arrays.toString(ex01.solution("xabcxdefxghi")));
+	}
+	
+
+}
