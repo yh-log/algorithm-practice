0e8dd44a5d460eb40a120eafa45b43f1b903d9e2
yh.kim <yihyun.22222@gmail.com>
2025-04-24 20:13:02 +0900

    04.24 코딩테스트 - 서울에서 김서방 찾기

---
+++ src/algorithm11/Ex03.java
@@ -0,0 +1,45 @@
+package algorithm11;
+
+import java.util.Arrays;
+
+/**
+ * 링크 https://school.programmers.co.kr/learn/courses/30/lessons/12919
+ */
+
+public class Ex03 {
+
+	/**
+	 * 문제 설명
+	 * 1. String 배열 중 "Kim" 의 위치를 찾아
+	 * 2. "김서방은 x에 있다" 는 String을 return
+	 */
+	
+	
+	// 풀이
+	// 시간 5.24ms ~ 8.13ms
+	public String solution(String[] seoul) {
+		
+		int x = 0;
+		
+		for(int i = 0; i < seoul.length; i++) {
+			if(seoul[i].contains("Kim")) {
+				x = i;
+				break;
+			}
+		}
+		
+		return 	"김서방은 " + x + "에 있다";
+	}
+	
+	
+	// 풀이 2. indexOf() 사용
+	// 시간 5.93ms ~ 7.82ms
+	
+	public String solution1(String[] seoul) {
+		
+		int x = Arrays.asList(seoul).indexOf("Kim");
+		
+		return 	"김서방은 " + x + "에 있다";
+	}
+	
+}
