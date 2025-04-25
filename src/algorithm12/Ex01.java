cf44cedc6b418bd9f977c784fefa1cb24989f5a6
yh.kim <yihyun.22222@gmail.com>
2025-04-25 11:57:18 +0900

    04.25 코딩 테스트 - JadenCase 문자열 만들기

---
+++ src/algorithm12/Ex01.java
@@ -0,0 +1,70 @@
+package algorithm12;
+
+import algorithm12.Ex01;
+
+/**
+ * 링크 https://school.programmers.co.kr/learn/courses/30/lessons/12951
+ */
+
+public class Ex01 {
+	
+	/**
+	 * 문제 설명 (Lv.2)
+	 * 1. 첫 문자가 대문자 그 뒤에는 소문자로
+	 * 2. 만약 첫 문자가 숫자일 경우 뒤에는 소문자
+	 * 3. 공백 문자가 연속으로 나올 수 있음 (\\s+ 정규식 필요)
+	 */
+
+	
+	// 풀이
+	// 시간 0.07ms ~ 0.17ms
+	public String solution(String s) {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		String[] arr = s.toLowerCase().split(" ", -1);
+		
+		for(int i = 0; i < arr.length; i++) {
+			
+			System.out.println(arr[i] +  " 공백 나와?");
+			
+			if(arr[i].length() > 0) {
+				sb.append(Character.toUpperCase(arr[i].charAt(0)))
+					.append(arr[i].substring(1));
+			}else {
+				sb.append(arr[i]);
+			}
+			
+			if(i < arr.length -1) {
+				sb.append(" ");
+			}
+		}
+				
+		return sb.toString();
+	}
+	
+	
+	// 풀이 2
+	// 시간 1.28ms ~ 3.74ms
+	public String soultion1(String s) {
+		
+		String answer = "";
+		String[] arr = s.toLowerCase().split("");
+		boolean flag = true;
+		
+		for(String str : arr) {
+			answer += flag ? str.toUpperCase() : str;
+			flag = str.equals(" ") ? true : false;
+		}
+		
+		return answer;
+	}
+	
+	
+	public static void main(String[] agrs) {
+		Ex01 ex01 = new Ex01();
+		System.out.println(ex01.solution("3pEoplE          unFollowed   me     "));
+		System.out.println(ex01.solution("f 1d 3d sdfse a        "));
+	}
+	
+}
