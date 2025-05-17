782f2636bd440686010a6deecd1c8a04b9dead3d
yh.kim <yihyun.22222@gmail.com>
2025-05-18 02:16:17 +0900

    05.18 코딩테스트 - 음수 찾기

---
+++ src/algorithm09/Ex02.java
@@ -0,0 +1,30 @@
+package algorithm09;
+
+/**
+ * 링크 https://school.programmers.co.kr/learn/courses/30/lessons/181896
+ */
+
+public class Ex02 {
+	
+	/**
+	 * 문제 설명
+	 * 1. 배열에서 음수가 나오는 인덱스를 return
+	 */
+	
+	// 풀이
+	// 시간 ms ~ ms
+	public int solution(int[] num_list) {
+		
+		int num = -1;
+		
+		for(int i = 0; i < num_list.length; i++) {
+			if(num_list[i] < 0) {
+				num = i;
+				break;
+			}
+		}
+		
+		return num;
+	}
+
+}
