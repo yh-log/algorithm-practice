e8e52007ad22947e993d980ae47643c4f41b896b
yh.kim <yihyun.22222@gmail.com>
2025-05-22 21:25:08 +0900

    05.22 코딩테스트 - 가장 가까운 1 찾기

---
+++ src/algorithm09/Ex07.java
@@ -0,0 +1,26 @@
+package algorithm09;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181898
+
+public class Ex07 {
+	
+	/**
+	 * 문제 설명
+	 * 1. 0과 1로만 이루어진 정수 배열이 주어질 때
+	 * 2. idx 보다 index가 크면서 값이 1인 가장 작은 인덱스를 return
+	 * 3. 없을 경우 -1을 return
+	 */
+	
+	// 풀이
+	// 시간 ms ~ ms
+	public int solution(int[] arr, int idx) {
+		
+		for(int i = idx; i < arr.length; i++) {
+			if(arr[i] == 1) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+}
