1c6fe336922411c524b0ade992a6f38347cadea0
yh.kim <yihyun.22222@gmail.com>
2025-07-21 23:46:11 +0900

    07/21 - 정수 반환

---
+++ src/algorithm11/Ex10.java
@@ -0,0 +1,20 @@
+package algorithm11;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/181850
+
+public class Ex10 {
+   
+   /*
+    * 문제 설명 (정수 부분)
+    * 1. double 값을 정수 부분만 return
+    */
+   
+   // 풀이
+   // 시간 0.03ms ~ 0.03ms
+   public int solution(double flo) {
+      
+//      return (int) Math.abs(flo);
+      return (int) flo; 
+   }
+
+}
