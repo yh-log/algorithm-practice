96c9486e2aad7db3313357d5cf0870dabfe9d718
yh.kim <yihyun.22222@gmail.com>
2025-08-02 10:57:47 +0900

    8/2 - 문자열, 정수 변환

---
+++ src/algorithm13/Ex02.java
@@ -0,0 +1,16 @@
+package algorithm13;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181848
+
+public class Ex02 {
+
+   /*
+    * 문제 설명 (문자열 정수로 변환)
+    * 1. 숫자로 이뤄진 문자열을 정수로 변환해 리턴
+    */
+   
+   public int solution(String n_str) {
+        return Integer.valueOf(n_str);
+   }
+   
+}
